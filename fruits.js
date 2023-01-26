631ad68df420ecdb7fc517dba709dc57220b0486
RojasSaenzJulian <105955487+RojasSaenzJulian@users.noreply.github.com>
2023-01-25 20:18:10 -0600

    Desarrollador 3

--- fruits.js
+++ fruits.js
@@ -1,6 +1,12 @@
 // Desarrollador 1
 let frutas = ["manzana", "plátano", "pera", "mango"];
+
 //Desarrollador 2
 function imprimiFrutas(){
 	console.log(frutas);
+}
+
+// Desarrollador 3
+function agregarFruta(fruta) {
+    frutas.push(fruta);
 }
